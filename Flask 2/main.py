8081821121a885a7df8f399eebab07dcded83833
MasonScarby <112995975+MasonScarby@users.noreply.github.com>
2024-04-14 11:47:29 -0400

    Final Summition

--- Flask 2/main.py
+++ Flask 2/main.py
@@ -1,122 +1,144 @@
-from imghdr import tests
-
-from flask import Flask, render_template, request, redirect
+import flask
+from flask import Flask, render_template, request, redirect, url_for, abort
 from sqlalchemy import create_engine, text
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 conn_str = 'mysql://root:Cookiebear1@localhost/flask'
 engine = create_engine(conn_str, echo = True)
 conn = engine.connect()
 
+
 #Create test
+# @app.route('/create_test', methods=['GET', 'POST'])
+# def create_test():
+#     if request.method == 'POST':
+#         user_id = request.form.get("teacher_id")
+#         test_name = request.form.get("test_name")
+#         conn.execute(text("INSERT INTO tests (teacher_id, test_name) VALUES (:teacher_id, :test_name)"), flask.request.form)
+#         conn.commit()
+#         return render_template("create_test.html")
+
 @app.route('/create_test', methods=['GET'])
-def create_get_test():
-    return render_template("create_test.html")
+def create_test():
+    return flask.render_template('create_test.html')
 
 @app.route('/create_test', methods=['POST'])
-def create_test():
-    test_id = request.form.get("test_id")
-    teacher_id = request.form.get("teacher_id")
-    test_name = request.form.get("test_name")
-    test_answer = request.form.get("test_answer")
-    test_question = request.form.get("test_question")
+def create_tests():
+        conn.execute(text("INSERT INTO tests (test_id, teacher_id, test_name) VALUES (:test_id, :teacher_id, :test_name)"), flask.request.form)
+        conn.commit()
+        return flask.render_template('create_question.html')
 
-    sql = text("INSERT INTO tests (test_id, teacher_id, test_name, test_answer) "
-               "VALUES (:test_id, :teacher_id, :test_name, :test_answer)")
-    conn.execute(sql, {"test_id": test_id, "teacher_id": teacher_id, "test_name": test_name, "test_answer": test_answer})
+#create question
+@app.route('/create_question', methods = ['GET'])
+def get_create_question():
+    return flask.render_template('create_question.html')
 
-    sql = text("INSERT INTO questions (test_question) VALUES (:test_question)")
-    conn.execute(sql, {"test_question": test_question})
+@app.route('/create_question', methods=['POST'])
+def create_question():
+    conn.execute(text("INSERT INTO tests (test_id, test_question, answer) VALUES (:test_id, :test_question, :answer)"),
+                 flask.request.form)
     conn.commit()
-
-    return render_template("create_test.html")
-
-#homepage
+    return render_template('create_question.html')
+#home
 @app.route('/')
 def homepage():
-    return render_template('create_acc.html')
+    return flask.render_template('create_acc.html')
 
 #accounts
 @app.route('/accounts_page', methods=['GET', 'POST'])
 def search():
-    if request.method == 'GET':
+    if flask.request.method == 'GET':
         all_accounts = conn.execute(text("SELECT * FROM users")).fetchall()
-        return render_template('accounts_page.html', info_type = all_accounts)
-    elif request.method == 'POST':
-        x = request.form['type']
+        return flask.render_template('accounts_page.html', info_type = all_accounts)
+    elif flask.request.method == 'POST':
+        x = flask.request.form['type']
         account_info = conn.execute(text("SELECT * FROM users WHERE type = :type"), {'type': x}).fetchall()
-        return render_template('accounts_page.html', info_type=account_info)
+        return flask.render_template('accounts_page.html', info_type=account_info)
 
 #Filter
 @app.route('/filter', methods=['GET'])
 def searches():
-    return render_template('filter.html', info_type=[])
+    return flask.render_template('filter.html', info_type=[])
 
 
 @app.route('/filter', methods=['POST'])
 def search_account():
-    x = request.form['type']
+    x = flask.request.form['type']
     account_info = conn.execute(text(f"SELECT * FROM users WHERE type = :type"), {'type': x}).fetchall()
-    return render_template('filter.html', info_type=account_info)
+    return flask.render_template('filter.html', info_type=account_info)
+
+#show tests
+@app.route('/show_test')
+def show_tests():
+    tests = conn.execute(text('select * from tests')).all()
+    return flask.render_template('show_tests.html', show_tests=tests)
 
 
 #create acc
 @app.route('/create_acc', methods=['GET'])
 def create_get_request():
-    return render_template('create_acc.html')
+    return flask.render_template('create_acc.html')
 
 
 @app.route('/create_acc', methods=['POST'])
-def create_boats():
-    conn.execute(text('INSERT into users Values (:user_id, :type, :name, :email, :username, :password)'), request.form)
+def create_account():
+    type = request.form.get('type')
+    name = request.form.get('name')
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    conn.execute(text(
+        'INSERT INTO users (type, name, email, username, password) VALUES (:type, :name, :email, :username, :password)'),
+                 {'type': type, 'name': name, 'email': email, 'username': username, 'password': password})
     conn.commit()
-    return render_template('create_acc.html')
+    return flask.render_template("create_acc.html")
 
 
 #take test
 @app.route('/take_test', methods=['GET'])
 def refresh():
-    return render_template('take_test.html')
-
+    return render_template('take_test.html', test_info=[])
 
 @app.route('/take_test', methods=['POST'])
 def get_test():
-    x = request.form['test_id']
-    boat = conn.execute(text(f'select * from tests where test_id = {x}'))
-    conn.execute(text(f"select * from tests where test_id = {x}"), request.form)
-    conn.commit()
-    return render_template('take_test.html', test_info=boat)
+    test_id = request.form.get('test_id')
+    tester_id = request.form.get('tester_id')
+
+    test_info = conn.execute(text('SELECT * FROM tests WHERE test_id = :id'), id=test_id).fetchall()
+
+    return render_template('take_test.html', test_info=test_info)
 
 
 #update
 @app.route('/update_test', methods=['GET'])
 def update():
-    return render_template('update.html')
+    return flask.render_template('update.html')
 
 
 @app.route('/update_test', methods=['POST'])
 def update_boat():
-    x = request.form['test_id']
-    name = request.form['teacher_id']
-    boat_type = request.form['test_name']
-    owner_id = request.form['test_answer']
+    x = flask.request.form['test_id']
+    name = flask.request.form['teacher_id']
+    boat_type = flask.request.form['test_name']
+    owner_id = flask.request.form['test_answer']
     boat = conn.execute(text(f'select * from tests where test_id = {x}'))
-    conn.execute(text(f"UPDATE tests SET test_id = :test_id, teacher_id = :teacher_id, test_name = :test_name WHERE {x} = :test_id "), request.form)
+    conn.execute(text(f"UPDATE tests SET teacher_id = :teacher_id, test_name = :test_name, test_answer = test_answer WHERE {x} = :test_id "), flask.request.form)
     conn.commit()
-    return render_template('update.html')
+    return flask.render_template('update.html')
 
 
 #delete
 @app.route('/delete_test', methods=["GET", "POST"])
 def delete_boats():
-    if request.method == "POST":
-        conn.execute(text("DELETE FROM tests WHERE test_id = :test_id"), request.form)
+    if flask.request.method == "POST":
+        conn.execute(text("DELETE FROM tests WHERE test_id = :test_id"), flask.request.form)
         conn.commit()
 
-        return redirect("/create_test")
+        return flask.redirect("/create_test")
     else:
-        return render_template("delete_test.html")
+        return flask.render_template("delete_test.html")
 
 
 if __name__ == '__main__':
